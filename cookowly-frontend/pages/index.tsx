b75f288a968968093784f2c5c2b9d52dd0b65fe6
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2022-10-13 16:51:42 +0200

    init playwright; init tailwind

--- cookowly-frontend/pages/index.tsx
+++ cookowly-frontend/pages/index.tsx
@@ -1,5 +1,27 @@
+import { Switch } from '@headlessui/react';
+import { useState } from 'react';
+
 function Home() {
-  return <h1 className="bg-red-500 text-blue.500">Hello Cookwoly</h1>;
+  const [enabled, setEnabled] = useState<boolean>(false);
+  return (
+    <>
+      <h1 className="bg-red-500 text-blue-500" data-pw="hello-world">
+        Hello Cookwoly
+      </h1>
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={`${enabled ? 'bg-blue-600' : 'bg-gray-200'} relative inline-flex h-6 w-11 items-center rounded-full`}
+      >
+        <span className="sr-only">Enable notifications</span>
+        <span
+          className={`${
+            enabled ? 'translate-x-6' : 'translate-x-1'
+          } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+        />
+      </Switch>
+    </>
+  );
 }
 
 export default Home;
--- cookowly-frontend/tailwind.config.js
+++ cookowly-frontend/tailwind.config.js
@@ -1,8 +1,98 @@
 /** @type {import('tailwindcss').Config} */
+
+const dangerColors = {
+  'danger-50': 'hsl(0, 100%, 100%)',
+  'danger-100': 'hsl(0, 100%, 100%)',
+  'danger-200': 'hsl(0, 100%, 100%)',
+  'danger-300': 'hsl(0, 100%, 100%)',
+  'danger-400': 'hsl(0, 100%, 100%)',
+  'danger-500': 'hsl(0, 100%, 100%)',
+  'danger-600': 'hsl(0, 100%, 100%)',
+  'danger-700': 'hsl(0, 100%, 100%)',
+  'danger-800': 'hsl(0, 100%, 100%)',
+  'danger-900': 'hsl(0, 100%, 100%)',
+};
+
+const warningColors = {
+  'warning-50': 'hsl(0, 100%, 100%)',
+  'warning-100': 'hsl(0, 100%, 100%)',
+  'warning-200': 'hsl(0, 100%, 100%)',
+  'warning-300': 'hsl(0, 100%, 100%)',
+  'warning-400': 'hsl(0, 100%, 100%)',
+  'warning-500': 'hsl(0, 100%, 100%)',
+  'warning-600': 'hsl(0, 100%, 100%)',
+  'warning-700': 'hsl(0, 100%, 100%)',
+  'warning-800': 'hsl(0, 100%, 100%)',
+  'warning-900': 'hsl(0, 100%, 100%)',
+};
+
+const successColors = {
+  'success-50': 'hsl(0, 100%, 100%)',
+  'success-100': 'hsl(0, 100%, 100%)',
+  'success-200': 'hsl(0, 100%, 100%)',
+  'success-300': 'hsl(0, 100%, 100%)',
+  'success-400': 'hsl(0, 100%, 100%)',
+  'success-500': 'hsl(0, 100%, 100%)',
+  'success-600': 'hsl(0, 100%, 100%)',
+  'success-700': 'hsl(0, 100%, 100%)',
+  'success-800': 'hsl(0, 100%, 100%)',
+  'success-900': 'hsl(0, 100%, 100%)',
+};
+
+const primaryColors = {
+  'primary-50': 'hsl(0, 100%, 100%)',
+  'primary-100': 'hsl(0, 100%, 100%)',
+  'primary-200': 'hsl(0, 100%, 100%)',
+  'primary-300': 'hsl(0, 100%, 100%)',
+  'primary-400': 'hsl(0, 100%, 100%)',
+  'primary-500': 'hsl(0, 100%, 100%)',
+  'primary-600': 'hsl(0, 100%, 100%)',
+  'primary-700': 'hsl(0, 100%, 100%)',
+  'primary-800': 'hsl(0, 100%, 100%)',
+  'primary-900': 'hsl(0, 100%, 100%)',
+};
+
+const accentColors = {
+  'accent-50': 'hsl(0, 100%, 100%)',
+  'accent-100': 'hsl(0, 100%, 100%)',
+  'accent-200': 'hsl(0, 100%, 100%)',
+  'accent-300': 'hsl(0, 100%, 100%)',
+  'accent-400': 'hsl(0, 100%, 100%)',
+  'accent-500': 'hsl(0, 100%, 100%)',
+  'accent-600': 'hsl(0, 100%, 100%)',
+  'accent-700': 'hsl(0, 100%, 100%)',
+  'accent-800': 'hsl(0, 100%, 100%)',
+  'accent-900': 'hsl(0, 100%, 100%)',
+};
+
+const neutralColors = {
+  'neutral-50': 'hsl(0, 100%, 100%)',
+  'neutral-100': 'hsl(0, 100%, 100%)',
+  'neutral-200': 'hsl(0, 100%, 100%)',
+  'neutral-300': 'hsl(0, 100%, 100%)',
+  'neutral-400': 'hsl(0, 100%, 100%)',
+  'neutral-500': 'hsl(0, 100%, 100%)',
+  'neutral-600': 'hsl(0, 100%, 100%)',
+  'neutral-700': 'hsl(0, 100%, 100%)',
+  'neutral-800': 'hsl(0, 100%, 100%)',
+  'neutral-900': 'hsl(0, 100%, 100%)',
+};
+
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...primaryColors,
+        ...accentColors,
+        ...neutralColors,
+        ...dangerColors,
+        ...warningColors,
+        ...successColors,
+      },
+      fontFamily: {},
+      spacing: {},
+    },
   },
   plugins: [],
 };
