3ac06ed19387d645a0bfd7a8a4ba56af93ced121
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2022-11-05 13:53:52 +0100

    add onClick

--- cookowly-frontend/src/components/PrimaryButton.tsx
+++ cookowly-frontend/src/components/PrimaryButton.tsx
@@ -6,9 +6,14 @@ interface IPrimaryButtonProps {
   isSubmitButton?: boolean;
   isDisabled?: boolean;
   icon?: JSX.Element | null;
+  onClick?: () => void;
 }
 
-const sizeStyles: { [Key in Size]: string } = { lg: 'px-8 py-4', md: 'px-4 py-2', sm: 'px-2 py-1' };
+const sizeStyles: { [Key in Size]: string } = {
+  lg: 'px-8 py-4',
+  md: 'px-4 py-2',
+  sm: 'px-2 py-1',
+};
 
 export function PrimaryButton({
   text,
@@ -16,10 +21,16 @@ export function PrimaryButton({
   isSubmitButton = false,
   isDisabled = false,
   icon = null,
+  onClick,
 }: IPrimaryButtonProps) {
   const styles = `${sizeStyles[size]} bg-primary-700 hover:bg-primary-800 active:bg-primary-900 text-primary-50 rounded`;
   return (
-    <button className={styles} type={isSubmitButton ? 'submit' : 'button'} disabled={isDisabled}>
+    <button
+      className={styles}
+      type={isSubmitButton ? 'submit' : 'button'}
+      disabled={isDisabled}
+      onClick={() => (onClick ? onClick() : null)}
+    >
       {icon && <span className="mr-4 h-6 w-6 inline-block align-middle">{icon}</span>}
       {text}
     </button>
