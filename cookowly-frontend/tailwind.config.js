e21d3072760ff277262778fb194bf79b9b6fd5f4
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2022-11-05 12:56:59 +0100

    Change folder structure

--- cookowly-frontend/tailwind.config.js
+++ cookowly-frontend/tailwind.config.js
@@ -79,7 +79,7 @@ const neutralColors = {
 };
 
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
