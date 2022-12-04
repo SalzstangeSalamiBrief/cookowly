f2cabb6eb507d986de157ccb65eb0638b0a8a868
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2022-12-04 13:56:23 +0100

    Add ErrorBoundary and text component

---
+++ cookowly-frontend/src/components/error-boundary/ErrorBoundary.tsx
@@ -0,0 +1,49 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+import { Button } from '../buttons/Button';
+import { Text } from '../Text/Text';
+
+interface IProps {
+  children: ReactNode;
+}
+
+interface IState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // eslint-disable-next-line no-console
+    console.error('Uncaught error: ', error, errorInfo);
+  }
+
+  render() {
+    const { state, props } = this;
+    if (state.hasError) {
+      return (
+        <div
+          className={`
+            my-4 mx-auto w-full md:w-1/2 lg:w-1/3 xl:w-1/4
+            h-max h- p-4 text-danger-50 bg-danger-900 rounded-md`}
+        >
+          <Text type="heading" as="h1" margin="my-4" size="xl" color="text-primary-50">
+            Something went wrong.
+          </Text>
+          <div className="flex justify-end">
+            <Button text="Try again" as="nextLink" href="/" variant="ghost" />
+          </div>
+        </div>
+      );
+    }
+
+    return props.children;
+  }
+}
--- cookowly-frontend/src/pages/_app.tsx
+++ cookowly-frontend/src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { Navigation } from '../components/navigation/Navigation';
 import { Sidebar } from '../components/sidebar/Sidebar';
 import { MainHeader } from '../components/main-header/mainHeader';
 import { useGetBreakpoints } from '../hooks/useGetBreakpoints';
+import { ErrorBoundary } from '../components/error-boundary/ErrorBoundary';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { isXl } = useGetBreakpoints();
@@ -14,14 +15,16 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <div className="bg-neutral-900 text-neutral-50 font-content flex">
-      <Navigation />
-      <main data-pw="main" className="flex-grow">
-        <MainHeader />
-        <div className="p-4">
-          <Component {...pageProps} />
-        </div>
-      </main>
-      {sidebarToRender}
+      <ErrorBoundary>
+        <Navigation />
+        <main data-pw="main" className="flex-grow">
+          <MainHeader />
+          <div className="p-4">
+            <Component {...pageProps} />
+          </div>
+        </main>
+        {sidebarToRender}
+      </ErrorBoundary>
     </div>
   );
 }
