c8af6557b9825f962df0f3eb6cc7afaff1876317
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2022-12-11 14:34:15 +0100

    add todo

--- cookowly-frontend/src/components/main-header/mainHeader.tsx
+++ cookowly-frontend/src/components/main-header/mainHeader.tsx
@@ -8,6 +8,7 @@ export function MainHeader() {
       className="p-4 border-b border-neutral-50/alpha-10 bg-neutral-900/alpha-97 font-title text-2xl sticky top-0"
       data-pw="main-header"
     >
+      {/* TODO REPLACE WITH TEXT COMPONENT AFTER THE CORRESPONDING PR IS MERGED INTO MAIN */}
       <h1>{currentRoute.displayName}</h1>
     </div>
   );
