95c1313b17e4e5e4b86c82e5a0c1fc906eaa8dfd
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2022-12-17 12:52:33 +0100

    Fix icon

--- cookowly-frontend/src/pages/_app.tsx
+++ cookowly-frontend/src/pages/_app.tsx
@@ -16,7 +16,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        {/* src favicon  https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
       <div className="bg-neutral-900 text-neutral-50 font-content flex">
         <Navigation />
