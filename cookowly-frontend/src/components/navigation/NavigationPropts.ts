66a091c142dcf5c0cbb6281c0538fa80ce620136
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2022-12-20 16:32:52 +0100

    add comments

--- cookowly-frontend/src/components/navigation/NavigationPropts.ts
+++ cookowly-frontend/src/components/navigation/NavigationPropts.ts
@@ -1,11 +1,23 @@
 import { ReactElement } from 'react';
 
 export interface INavigationLink {
+  /**
+   * the name of the link that should be rendered
+   */
   displayName: string;
+  /**
+   * the route of the link
+   */
   path: string;
+  /**
+   * a icon to render
+   */
   icon: ReactElement;
 }
 
 export interface INavigationProps {
+  /**
+   * all links
+   */
   links: INavigationLink[];
 }
