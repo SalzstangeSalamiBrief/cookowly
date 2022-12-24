66c8745d3f7dbcc037e186326e50f266149b5001
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2022-12-24 13:05:51 +0100

    fix main header; fix comments

--- cookowly-frontend/src/components/main-header/mainHeader.tsx
+++ cookowly-frontend/src/components/main-header/mainHeader.tsx
@@ -1,4 +1,5 @@
 import { useGetCurrentRoute } from '../../hooks/useGetCurrentRoute';
+import { Text } from '../Text/Text';
 
 export function MainHeader() {
   const currentRoute = useGetCurrentRoute();
@@ -8,8 +9,9 @@ export function MainHeader() {
       className="p-4 border-b border-neutral-50/alpha-10 bg-neutral-900/alpha-97 font-title text-2xl sticky top-0"
       data-pw="main-header"
     >
-      {/* TODO REPLACE WITH TEXT COMPONENT AFTER THE CORRESPONDING PR IS MERGED INTO MAIN */}
-      <h1>{currentRoute.displayName}</h1>
+      <Text type="heading" as="h1" size="2xl">
+        {currentRoute.displayName}
+      </Text>
     </div>
   );
 }
