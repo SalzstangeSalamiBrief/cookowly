514a7f620c1bef905cacb58d2496f77c18f7e09f
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2022-12-24 09:16:01 +0100

    fix 404 message

--- cookowly-frontend/src/pages/404.tsx
+++ cookowly-frontend/src/pages/404.tsx
@@ -7,7 +7,7 @@ export default function Custom404page() {
     <div className="flex flex-col items-center">
       <h1 className="mb-4">404 - Page Not Found</h1>
       <BrandIcon />
-      <Text margin="my-4">The request page could does not exist.</Text>
+      <Text margin="my-4">The requested page does not exist.</Text>
       <div className="flex">
         <Button text="Try again" onClick={() => document.location.reload()} variant="action" />
         <Button text="Home" as="nextLink" href="/" variant="action" />
